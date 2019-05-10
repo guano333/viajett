4c8fa436d8c55f272895f4d93a75c8c906fd6184
Tim Swords <swords.tim@gmail.com>
2019-05-10 00:22:12 -0500

    Base Project setup

---
+++ Viajett/Viajett/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Viajett.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult LogTime()
+        {
+            return View();
+        }
+
+        public ActionResult MyTasks()
+        {
+            return View();
+        }
+
+        public ActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
