86cee20a64eda7510f9ae622fcf9756772577369
Tim Swords <swords.tim@gmail.com>
2019-05-10 11:42:27 -0500

    Configured TypeScript

--- Viajett/Viajett/App_Start/BundleConfig.cs
+++ Viajett/Viajett/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace Viajett
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/viajett").Include(
+                      "~/Scripts/viajett/viajett.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
