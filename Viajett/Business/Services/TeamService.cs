b1755ced86f04fe6f6bd23487528459dca0f9781
DOTFOODS\JMiller <jmiller@dotfoods.com>
2019-05-15 14:22:56 -0500

    Team Business Service

--- Viajett/Business/Services/TeamService.cs
+++ Viajett/Business/Services/TeamService.cs
@@ -1,24 +1,45 @@
 ﻿using Business.Interfaces;
+using Data.Interfaces;
 using Domain;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Business.Services
 {
     public class TeamService : ITeamService
     {
+        private readonly ITeamRepository _teamRepository;
+
+        public TeamService(ITeamRepository teamRepository)
+        {
+            _teamRepository = teamRepository;
+        }
+
         public void DeleteTeam(Team source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                _teamRepository.Delete(source.TeamId);
+            }
         }
 
         public void SetManager(Team source, Employee manager)
         {
-            throw new NotImplementedException();
+            if (source != null && manager != null)
+            {
+                source.ManagerId = manager.EmployeeId;
+                _teamRepository.Upsert(source);
+            }
         }
 
         public Team UpsertTeam(Team source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                _teamRepository.Upsert(source);
+            }
+            return source;
         }
     }
 }
