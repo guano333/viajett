b28e935dd2caffd681c023f9b691941f7228c061
Tim Swords <swords.tim@gmail.com>
2019-05-15 13:53:56 -0500

    Employee Business Service

--- Viajett/Business/Services/EmployeeService.cs
+++ Viajett/Business/Services/EmployeeService.cs
@@ -1,35 +1,57 @@
 ﻿using Business.Interfaces;
+using Data.Interfaces;
 using Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Business.Services
 {
     public class EmployeeService : IEmployeeService
     {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
         public void DeleteEmployee(Employee source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                _employeeRepository.Delete(source.EmployeeId);
+            }
         }
 
         public void DisableEmployee(Employee source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                source.IsActive = false;
+                _employeeRepository.Upsert(source);
+            }
         }
 
         public void EnableEmployee(Employee source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                source.IsActive = true;
+                _employeeRepository.Upsert(source);
+            }
         }
 
         public IEnumerable<Employee> GetActiveEmployeesByTeam(Team team)
         {
-            throw new NotImplementedException();
+            return _employeeRepository.GetAll()
+                .Where(x => x.TeamId == team.TeamId && x.IsActive == true).ToList();
         }
 
         public IEnumerable<Employee> GetAllEmployeesByTeam(Team team)
         {
-            throw new NotImplementedException();
+            return _employeeRepository.GetAll()
+                .Where(x => x.TeamId == team.TeamId).ToList();
         }
 
         public Employee GetCurrentEmployee(string emailAddress)
@@ -39,12 +61,20 @@ namespace Business.Services
 
         public void ReassignEmployee(Employee source, Team newTeam)
         {
-            throw new NotImplementedException();
+            if (source != null && newTeam != null)
+            {
+                source.TeamId = newTeam.TeamId;
+                _employeeRepository.Upsert(source);
+            }
         }
 
         public Employee UpsertEmployee(Employee source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                _employeeRepository.Upsert(source);
+            }
+            return source;
         }
     }
 }
