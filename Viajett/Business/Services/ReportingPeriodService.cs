c0f30700734de431dcb3e97cc637b9b9d9f22ffc
Tim Swords <swords.tim@gmail.com>
2019-05-15 14:26:05 -0500

    Reporting Period Business Service

--- Viajett/Business/Services/ReportingPeriodService.cs
+++ Viajett/Business/Services/ReportingPeriodService.cs
@@ -1,34 +1,53 @@
 ﻿using Business.Interfaces;
+using Data.Interfaces;
 using Domain;
 using System;
+using System.Linq;
 
 namespace Business.Services
 {
     public class ReportingPeriodService : IReportingPeriodService
     {
+        private readonly IReportingPeriodRepository _reportingPeriodRepository;
+
+        public ReportingPeriodService(IReportingPeriodRepository reportingPeriodRepository)
+        {
+            _reportingPeriodRepository = reportingPeriodRepository;
+        }
+
         public void DeleteReportingPeriod(ReportingPeriod source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                _reportingPeriodRepository.Delete(source.ReportingPeriodId);
+            }
         }
 
         public ReportingPeriod GetCurrentReportingPeriod()
         {
-            throw new NotImplementedException();
+            return _reportingPeriodRepository.GetAll()
+                .Where(x => x.StartDate <= DateTime.Now.Date 
+                && x.EndDate > DateTime.Now.Date).SingleOrDefault();
         }
 
         public ReportingPeriod GetReportingPeriodByYearAndPeriodNumber(int year, int reportingPeriodNumber)
         {
-            throw new NotImplementedException();
+            return _reportingPeriodRepository.GetAll()
+                .Where(x => x.Year == year && x.PeriodNumber == reportingPeriodNumber).SingleOrDefault();
         }
 
         public int GetTotalHoursAvailableInReportingPeriod(ReportingPeriod reportingPeriod)
         {
-            throw new NotImplementedException();
+            return 160;
         }
 
         public ReportingPeriod UpsertReportingPeriod(ReportingPeriod source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                _reportingPeriodRepository.Upsert(source);
+            }
+            return source;
         }
     }
 }
