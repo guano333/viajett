adf45846671fce996fcdf1dbae801c4cc6407a0d
Tim Swords <swords.tim@gmail.com>
2019-05-15 12:26:45 -0500

    TaskCategory and TaskEntry Repositories

--- Viajett/Data/Repositories/TaskCategoryRepository.cs
+++ Viajett/Data/Repositories/TaskCategoryRepository.cs
@@ -2,6 +2,7 @@
 using Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Data.Repositories
 {
@@ -9,22 +10,60 @@ namespace Data.Repositories
     {
         public void Delete(Guid taskCategoryId)
         {
-            throw new NotImplementedException();
+            using (var context = new Viajett())
+            {
+                var taskCategory = context.TaskCategories.Where(x => x.TaskCategoryId == taskCategoryId).FirstOrDefault();
+                if (taskCategory != null)
+                {
+                    context.TaskCategories.Remove(taskCategory);
+                    context.SaveChanges();
+                }
+            }
         }
 
         public IEnumerable<TaskCategory> GetAll()
         {
-            throw new NotImplementedException();
+            using (var context = new Viajett())
+            {
+                return context.TaskCategories.ToList();
+            }
         }
 
-        public TaskCategory GetById(Guid taskCategory)
+        public TaskCategory GetById(Guid taskCategoryId)
         {
-            throw new NotImplementedException();
+            using (var context = new Viajett())
+            {
+                var taskCategory = context.TaskCategories.Where(x => x.TaskCategoryId == taskCategoryId).FirstOrDefault();
+                return taskCategory;
+            }
+
         }
 
         public TaskCategory Upsert(TaskCategory source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                using (var context = new Viajett())
+                {
+                    var taskCategory = context.TaskCategories.Where(x => x.TaskCategoryId == source.TaskCategoryId).FirstOrDefault();
+                    if (taskCategory != null)
+                    {
+                        //update
+                        taskCategory.CategoryName = source.CategoryName;
+                        context.SaveChanges();
+                        return taskCategory;
+                    }
+                    else
+                    {
+                        //insert
+                        context.TaskCategories.Add(source);
+                        context.SaveChanges();
+                        return taskCategory;
+                    }
+                }
+            }
+            return null;
+
         }
     }
 }
--- Viajett/Data/Repositories/TaskEntryRepository.cs
+++ Viajett/Data/Repositories/TaskEntryRepository.cs
@@ -2,29 +2,71 @@
 using Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Data.Repositories
 {
     public class TaskEntryRepository : ITaskEntryRepository
     {
-        public void Delete(Guid TaskEntryId)
+        public void Delete(Guid taskEntryId)
         {
-            throw new NotImplementedException();
+            using (var context = new Viajett())
+            {
+                var taskEntry = context.TaskEntries.Where(x => x.TaskEntryId == taskEntryId).FirstOrDefault();
+                if (taskEntry != null)
+                {
+                    context.TaskEntries.Remove(taskEntry);
+                    context.SaveChanges();
+                }
+            }
         }
 
         public IEnumerable<TaskEntry> GetAll()
         {
-            throw new NotImplementedException();
+            using (var context = new Viajett())
+            {
+                return context.TaskEntries.ToList();
+            }
+
         }
 
-        public TaskEntry GetById(Guid taskEntry)
+        public TaskEntry GetById(Guid taskEntryId)
         {
-            throw new NotImplementedException();
+            using (var context = new Viajett())
+            {
+                var taskEntry = context.TaskEntries.Where(x => x.TaskEntryId == taskEntryId).FirstOrDefault();
+                return taskEntry;
+            }
         }
 
         public TaskEntry Upsert(TaskEntry source)
         {
-            throw new NotImplementedException();
+            if (source != null)
+            {
+                using (var context = new Viajett())
+                {
+                    var taskEntry = context.TaskEntries.Where(x => x.TaskEntryId == source.TaskEntryId).FirstOrDefault();
+                    if (taskEntry != null)
+                    {
+                        //update
+                        taskEntry.EmployeeId = source.EmployeeId;
+                        taskEntry.ReportingPeriodId = source.ReportingPeriodId;
+                        taskEntry.TaskId = source.TaskId;
+                        taskEntry.Hours = source.Hours;
+                        context.SaveChanges();
+                        return taskEntry;
+                    }
+                    else
+                    {
+                        //insert
+                        context.TaskEntries.Add(source);
+                        context.SaveChanges();
+                        return taskEntry;
+                    }
+                }
+            }
+            return null;
+
         }
     }
 }
